fce0357e7aa2e54e36b77785ee9fb256ebf232a3
XG <365557696@qq.com>
2024-03-11 16:11:25 +0800

    变量名字 增加业务特征，要有辨识度

--- HelloWorld/HelloWorld/AppDelegate.m
+++ HelloWorld/HelloWorld/AppDelegate.m
@@ -20,18 +20,18 @@
     self.window= [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
     
     //ViewController@"新闻"
-    ViewController *viewController1=[[ViewController alloc] init];
+    ViewController *newsViewController=[[ViewController alloc] init];
     UINavigationController *navigationController1=[[UINavigationController alloc] initWithRootViewController:viewController1];
     navigationController1.view.backgroundColor=[UIColor whiteColor];
     navigationController1.tabBarItem.title=@"新闻";
     
     //PictureViewController@"图片"
-    PictureViewController *viewController2=[[PictureViewController alloc] init];
+    PictureViewController *pictureViewController=[[PictureViewController alloc] init];
     UINavigationController *navigationController2=[[UINavigationController alloc] initWithRootViewController:viewController2];
     navigationController2.tabBarItem.title=@"图片";
     
     //UIViewController@"我的"
-    UIViewController *controller3=[[UIViewController alloc] init];
+    UIViewController *profileViewController=[[UIViewController alloc] init];
     controller3.view.backgroundColor=[UIColor yellowColor];
     controller3.tabBarItem.title=@"我的";
     
